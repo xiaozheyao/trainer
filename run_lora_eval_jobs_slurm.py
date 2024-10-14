ee6580c4a4915ea9111d83606d88c44d90fae3ad
Xiaozhe Yao <askxzyao@gmail.com>
2024-10-14 11:40:32 +0000

    minor

--- run_lora_eval_jobs_slurm.py
+++ run_lora_eval_jobs_slurm.py
@@ -1,17 +1,15 @@
 import json
 
-def eval(args):
+def eval():
     with open("jobs/eval.jsonl", "r") as f:
         eval_jobs = [json.loads(line) for line in f.readlines()]
     for ejob in eval_jobs:
         base_model = ejob['ft_config']['model_name']
-        job = f"python /xyao/code/trainer/eval_lora.py --ckpt-path {ejob['ckpt_path']} --base-model {base_model} --test-set {args.test_set}"
+        print(ejob)
+        job = f"python /xyao/code/trainer/eval_lora.py --ckpt-path {ejob['ckpt_path']} --base-model {base_model} --test-set {ejob['ft_config']['test_path']}"
         print(job)
         exit(0)
     
 
 if __name__ =="__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    eval(args)
+    eval()
