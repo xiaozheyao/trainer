c180dee335c0df935df9fb508cc925118d194d4b
Xiaozhe Yao <askxzyao@gmail.com>
2024-10-14 11:39:03 +0000

    minor

--- run_lora_eval_jobs_slurm.py
+++ run_lora_eval_jobs_slurm.py
@@ -1,22 +1,6 @@
-import os
 import json
-import peft
-import torch
-import transformers
-from tqdm import tqdm
-
-def build_prompts(args):
-    data = []
-    with open(args.test_set, "r") as f:
-        prompts = [json.loads(line) for line in f.readlines()]
-    for prompt in prompts:
-        input_text = prompt["input"].split("<START_A>")[0] + "<START_A>"
-        output_text = "<START_A>" + prompt["input"].split("<START_A>")[1]
-        data.append({"input": input_text, "output": output_text})
-    return data
 
 def eval(args):
-    STOP_TOKEN = "<END_A>"
     with open("jobs/eval.jsonl", "r") as f:
         eval_jobs = [json.loads(line) for line in f.readlines()]
     for ejob in eval_jobs:
