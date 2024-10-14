477a2d043ed99a19aa89a8d7abd45ff61f4b3f2c
Xiaozhe Yao <askxzyao@gmail.com>
2024-10-14 11:52:16 +0000

    minor

--- run_lora_eval_jobs_slurm.py
+++ run_lora_eval_jobs_slurm.py
@@ -1,14 +1,20 @@
 import json
-
+import os
 def eval():
     with open("jobs/eval.jsonl", "r") as f:
         eval_jobs = [json.loads(line) for line in f.readlines()]
+    jobs = []
     for ejob in eval_jobs:
         base_model = ejob['ft_config']['model_name']
-        print(ejob)
+        
         job = f"python /xyao/code/trainer/eval_lora.py --ckpt-path {ejob['ckpt_path']} --base-model {base_model} --test-set {ejob['ft_config']['test_path']}"
-        print(job)
-        exit(0)
+        jobs.append(job)
+    for i, job in enumerate(jobs):
+        job_id = i % 10
+        job = f'sbatch -A a09 --reservation=sai-shared --job-name ft-13b-{job_id} --dependency singleton --environment trainer --output logs/%A.out --wrap="cd /xyao/code/trainer && {job}"'
+        if i<5:
+            print(job)
+        os.system(job)
     
 
 if __name__ =="__main__":
